e3054626d85386454c7f3e2a5aa6108dbf32818a
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-03-23 16:14:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,15 @@ input.onButtonPressed(Button.B, function () {
 })
 let asteroid: game.LedSprite = null
 let sen1: game.LedSprite = null
+let sen4: game.LedSprite = null
+let sen3: game.LedSprite = null
 let sen2: game.LedSprite = null
 let ship: game.LedSprite = null
 ship = game.createSprite(2, 4)
 ship.turn(Direction.Left, 90)
 game.setLife(5)
+basic.showIcon(IconNames.Target)
+basic.showString("Treasure Planet")
 basic.forever(function () {
     sen2 = game.createSprite(4, 1)
     basic.pause(500)
@@ -23,6 +27,30 @@ basic.forever(function () {
         basic.pause(500)
     }
     sen2.delete()
+})
+basic.forever(function () {
+    sen3 = game.createSprite(0, 3)
+    basic.pause(500)
+    while (true) {
+        sen3.move(1)
+        if (sen3.isTouching(ship)) {
+            game.removeLife(1)
+        }
+        sen3.ifOnEdgeBounce()
+        basic.pause(200 + 10 * randint(0, 10))
+    }
+})
+basic.forever(function () {
+    sen4 = game.createSprite(3, 2)
+    basic.pause(500)
+    while (true) {
+        sen4.move(1)
+        if (sen4.isTouching(ship)) {
+            game.removeLife(1)
+        }
+        sen4.ifOnEdgeBounce()
+        basic.pause(200 + 10 * randint(0, 10))
+    }
 })
 basic.forever(function () {
     sen1 = game.createSprite(0, 1)
